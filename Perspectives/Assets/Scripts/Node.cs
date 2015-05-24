968bd0ad027c994eadbca4656a6eca54cad1754d
Joe Bush <arcandio@gmail.com>
2015-05-24 11:10:34 -0500

    just adding ideas for drag and drop

--- Perspectives/Assets/Scripts/Node.cs
+++ Perspectives/Assets/Scripts/Node.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 public class Node : Element {
 	public NodeType nodeType = NodeType.None;
 	public Dictionary<string, Vector2> perspectivePositions;
+
+	// do drag and drop
+    //http://answers.unity3d.com/questions/847941/unity-46-drag-and-drop.html
 }
 public enum NodeType {
 	None,
